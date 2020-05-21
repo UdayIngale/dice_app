fef26a64db7203a4acb21cb3b0d4f56fce527456
UdayIngale <64007438+UdayIngale@users.noreply.github.com>
2020-05-21 23:40:25 +0530

    added dice images

--- Assets/Javascript/random.js
+++ Assets/Javascript/random.js
@@ -1,13 +1,62 @@
-
-
-let genrateRandomNo = function(){
-    let randmNo = Math.random() * 6 + 1
-    let newNo = Math.floor(randmNo)
+let image
+let genrateRandomNo = function( ) {
+    randmNo = Math.random() * 6 + 1
+    newNo = Math.floor(randmNo) 
     console.log(newNo)
+    
+//  let change = function() {
+//     image = document.getElementById('imag');
+//     console.log(image)
+//  }
 
-    document.getElementById("para").innerHTML = newNo;
+if (newNo == 1) {
+        image = document.getElementById('imag');
+           
+        image.src = 'Assets/Dices/1.png'
+    } 
+    
 
+else if (newNo == 2) {
+        image = document.getElementById('imag');
+        
+        image.src = 'Assets/Dices/2.png' 
+    } 
+    
+else if (newNo == 3) {
+        image = document.getElementById('imag');
+          
+        image.src = 'Assets/Dices/3.png' 
+    }   
+     
+
+else if (newNo == 4) {
+        image = document.getElementById('imag');
+          
+        image.src = 'Assets/Dices/4.png'
+    }   
+    
+
+else if (newNo == 5) {
+    
+        image = document.getElementById('imag');
+         
+        image.src = 'Assets/Dices/5.png'
+    }   
+    
+
+else   {
+    
+        image = document.getElementById('imag');
+        
+        image.src = 'Assets/Dices/6.png' 
+    }   
+    
 }
-let resetRandomNo = function(){
-    document.getElementById("para").innerHTML = '';
+
+
+
+let resetRandomNo = function() {
+    var none = document.getElementById("imag")
+    none.src = ""
 }
+
